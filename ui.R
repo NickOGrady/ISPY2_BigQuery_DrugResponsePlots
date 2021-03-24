1667d8d4256d457c745a09d82696b19660c56343
Nick O'Grady <58996597+ogradyng@users.noreply.github.com>
2021-03-24 11:52:50 -0700

    Update ui.r
    Remove switch/case. Improve efficiency.

--- ui.R
+++ ui.R
@@ -4,10 +4,25 @@ library(shiny)
 library(shinyWidgets)
 
 # Enter drug information here
-myarms = c("","DRUG_A","Drug_B", "Drug_C")
+# Human Readable on left, column names on right
+myarms = c("",
+           "DRUG_A" = "'<<Drug_A column name from  BQ table>>'",
+           "DRUG_B" = "'<<Drug_B column name from BQ Table'",
+           "Drug_C" = "'<<Drug_C coumna name from BQ Table'")
+
 # Enter in clinical variables here
-mychoices = c("HER2-", "HER2+", "HR-", "HR+", "MP_0", "MP_1", "pCR_0", "pCR_1", 
-              "RCB_I", "RCB_II", "RCB_III")
+# human readable on left, column names on right
+mychoices = c("HER2-" = "HER2 = '0'",
+              "HER2+" = "HER2 = '1'", 
+              "HR-" = "HR = '0'",
+              "HR+" = "HR = '1'",
+              "MP_0" = "MP = '0'",
+              "MP_1" = "MP = '1'", 
+              "pCR_0" = "Admin_PCR = '0'",
+              "pCR_1" = "Admin_PCR = '1'", 
+              "RCB_I" = "RCB_Class = 'I'", 
+              "RCB_II" = "RCB_Class = 'II'", 
+              "RCB_III" = "RCB_Class = 'III'")
 
 # Page Layout, titles, sidebar info
 ui <- fluidPage(
